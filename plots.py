e58b15fbad32bb7a8c728d0eb76d228a71438e40
Darius-ss10 <darius.muntean@student.uclouvain.be>
2023-03-11 13:06:22 +0100

    pakets_per_minute.txt + plot

--- plots.py
+++ plots.py
@@ -3,7 +3,11 @@ import os
 import matplotlib.pyplot as plt
 
 dir_context = ["/ethernet", "/ethernet_web", "/shared_connection", "/wifi_eduroam"]
-path_pwd = "/home/aperence/Documents/Analyse-d-application-reseau-Discord"
+# Anthony
+#path_pwd = "/home/aperence/Documents/Analyse-d-application-reseau-Discord"
+
+# Darius
+path_pwd = "/home/darius/Desktop/Analyse-d-application-reseau-Discord"
 files = []
 for dir in dir_context:
     path = os.path.realpath(path_pwd + dir)
@@ -35,8 +39,38 @@ def get_packets_per_minute(files):
     
     for key in scenarios:
         scenarios[key] = scenarios[key] / len(dir_context)
+
+    # Write to file
+    with open("packets_per_minute.txt", "w") as f:
+        for key in scenarios:
+            f.write(key + " : " + str(scenarios[key]) + "\n")
     
     return scenarios
 
-scenarios = get_packets_per_minute(files)
-print(scenarios)
+#scenarios = get_packets_per_minute(files)
+#print(scenarios)
+
+def plot_packets_per_minute(filename):
+    # Read the file
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        scenarios = {}
+        for line in lines:
+            line = line.split(" : ")
+            scenarios[line[0]] = float(line[1])
+    x = scenarios.keys()
+    y = scenarios.values()
+
+    # Create the bar chart
+    plt.bar(x, y)
+
+    # Add labels and title
+    plt.xlabel('Scénario')
+    plt.ylabel('Paquets par minute')
+    plt.title('Paquets par minute par scénario')
+
+    # Show the plot
+    plt.savefig("packets_per_minute.png")
+    plt.show()
+
+plot_packets_per_minute("packets_per_minute.txt")
